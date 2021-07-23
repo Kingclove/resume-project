51d193ab04b4a1151542ca15509b6fd198108da5
Kyle Hibbert <khibbert@mcsystems.com>
2021-07-23 11:17:51 -0500

    Create dog example

--- Program.cs
+++ Program.cs
@@ -6,36 +6,65 @@ namespace resume_project
     {
         static void Main(string[] args)
         {
-            string[] work_exp = new string[5];
-            string[] education= new string[5];
-            string[] skills = new string[5];
-            int i;
-            Console.Write("Name:"); 
-            var name = Console.ReadLine();
-            Console.Write("Address:");
-            var address = Console.ReadLine();
-            Console.Write("Telephone number:");
-            var telenum = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Email Address");
-            var email = Console.ReadLine();
-            Console.WriteLine("Summary paragraph:");
-            var summary = Console.ReadLine();
+            //Create new instance of dog
+            var newDog = new Dog();
+            Console.WriteLine("Enter Dog's name:");
+            newDog.Name = Console.ReadLine();
+            Console.WriteLine("Enter dog's breed:");
+            newDog.DogBreed = Console.ReadLine();
+            Console.WriteLine("Enter dogs age:");
+            newDog.Age = Convert.ToInt32(Console.ReadLine());
 
-            for (i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Work Experience:");
-                work_exp[i] = Console.ReadLine();
-            }
-            for (i = 0; i < 2; i++)
-            {
-                Console.WriteLine("Education:");
-                education[i] = Console.ReadLine();
-            }
-            for (i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Skills:");
-                skills[i] = Console.ReadLine();
-            }
+
+
+            var newCat = new Cat();
+            Console.WriteLine("Enter Cat's name:");
+            newCat.Name = Console.ReadLine();
+            Console.WriteLine("Enter Cat's color:");
+            newCat.Colour = Console.ReadLine();
+            Console.WriteLine("Enter Cat's age:");
+            newCat.Age = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(newDog.Name);
+            Console.WriteLine("============");
+
+            Console.WriteLine(newDog.GetDescription());
+            Console.WriteLine("");
+
+            Console.WriteLine(newCat.Name);
+            Console.WriteLine("============");
+
+            Console.WriteLine(newCat.GetDescription());
         }
     }
+
+
+
+    public class Dog :Animal
+    {
+        public string DogBreed { get; set; }
+        public string GetDescription()
+        {
+            //Same as return Name + "is a " + Breed + ". He is " + Age + " years old.";
+            return $"{Name} is a {DogBreed}. He is {Age} years old.";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public string Colour { get; set; }
+        public string GetDescription()
+        {
+            return $"{Name} is a {Colour} cat. He is {Age} years old.";
+        }
+    }
+
+    public class Animal
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+
 }
